6bfe7470427517a705ee3b2e09f9b28e2a244c40
Arthur Smalle <arthur@smalle.be>
2024-03-20 15:44:41 +0100

    Vakwerk Class toegevoegd deze Class combineert Graphs en punten om een vakwerk voor te stellen init met 3 basis punten belangrijk voor de opdracht heeft een functie die de knooppunten print

--- Vakwerk uitreken.py
+++ Vakwerk uitreken.py
@@ -64,6 +64,9 @@ class KnoopPunt:
             print("Position change aborted")
             return
         return
+    def change_index(self,index):
+        self.m_graph_index :int = index
+        return
 
     def test_print(self):
         print("Graph index", self.m_graph_index)
@@ -71,6 +74,46 @@ class KnoopPunt:
         print("force: ", self.m_force)
         return
 
+class Vakwerk:
+    def __init__(self, extradistance=0, extraKnooppunten= None, Verbindingen = None):
+        # lijst met alle knooppunten
+        self.m_knooppunten = []
+        # Init graph met verbindingen tussen alle knooppunten
+        if extraKnooppunten != None:
+            self.m_verbindingen = Graph(3 + len(extraKnooppunten), False)
+        else:
+            self.m_verbindingen = Graph(3,False)
+
+
+        # basispunten
+        steunpunt0 = KnoopPunt(0,0,0,0,0)
+        steunpunt1 = KnoopPunt(90 + extradistance * 2,0,0,0,1)
+        punthoek = KnoopPunt(45 + extradistance,40,0,0,2)
+        self.m_knooppunten.append(steunpunt0)
+        self.m_knooppunten.append(steunpunt1)
+        self.m_knooppunten.append(punthoek)
+        # verbindingen tussen basispunten
+        self.m_verbindingen.add_edge(0,1)
+        self.m_verbindingen.add_edge(0,2)
+        self.m_verbindingen.add_edge(1,2)
+
+
+        # voeg de extra knooppunten toe aan de lijst
+        if extraKnooppunten != None:
+            start_index :int = 2  # index vanaf waar de prebuild punten stoppen
+            for i in range(len(extraKnooppunten)):
+                extraKnooppunten[i].change_index(i + start_index) # change index (index moet veranderd worden zodat het klopt in dit vakwerk)
+                self.m_knooppunten.append(extraKnooppunten[i])
+
+    def print_knooppunten(self,index=None):
+        if index == None:
+            for i in range(len(self.m_knooppunten)):
+                print("+---------------------------+")
+                self.m_knooppunten[i].test_print()
+                print(self.m_verbindingen.m_adj_list[i])
+        else:
+            self.m_knooppunten[index].test_print()
+            print(self.m_verbindingen.m_adj_list[index])
 
 def solveSystem(F1 = 0 , F2 = 0, S1 = 0, S2 = 0, F3 = None, S3 = 0, F4 = None, S4 = 0):
     if F3 == None:
